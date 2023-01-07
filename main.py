09a573594daac7760c535846bb21caac9556ed08
avianage <89604905+avianage@users.noreply.github.com>
2023-01-08 01:13:00 +0530

    update_1

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-#123
+import discord
+from discord.ext import commands
+
+import os
+
+client = commands.Bot(command_prefix="$")
+
+
+@client.event
+async def on_ready():
+    print('We have logged as {0.user}'.format(client))
+
+
+client.run(os.getenv('TOKEN'))
